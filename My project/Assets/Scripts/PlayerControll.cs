410c7184f8a2dc433bbd288fbb121d8df778b15b
Nilbog24 <19342@wc.school.nz>
2022-05-02 20:45:13 +1200

    Fixed some movement bugs.

--- My project/Assets/Scripts/PlayerControll.cs
+++ My project/Assets/Scripts/PlayerControll.cs
@@ -18,12 +18,12 @@ public class PlayerControll : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        if(horizontalInput >= 0)
+        if(horizontalInput > 0)
             {
                 transform.eulerAngles = new Vector3(0, 0, 0);
                 transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
             }
-        if(horizontalInput < 0)
+        if(horizontalInput <= 0)
             {
                 transform.eulerAngles = new Vector3(0, 180, 0);
                 transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed * -1);
