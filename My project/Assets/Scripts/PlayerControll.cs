bf51dd1abd0b237b7ee7bb73cdcd7a5aff45ad7d
Nilbog24 <19342@wc.school.nz>
2022-05-03 12:36:02 +1200

    Added jump code

--- My project/Assets/Scripts/PlayerControll.cs
+++ My project/Assets/Scripts/PlayerControll.cs
@@ -6,27 +6,38 @@ public class PlayerControll : MonoBehaviour
 {
     public float speed = 5.0f;
     public float horizontalInput;
+    public float veritcalInput;
+    private Rigidbody playerRb;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        veritcalInput = Input.GetAxis("Vertical");
         if(horizontalInput > 0)
-            {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-                transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-            }
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        }
         if(horizontalInput <= 0)
-            {
-                transform.eulerAngles = new Vector3(0, 180, 0);
-                transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed * -1);
-            }
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed * -1);
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ladder")
+        {
+            playerRb.AddForce(transform.up * veritcalInput * speed);
+        }
     }
 }
